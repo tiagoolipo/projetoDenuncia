439e83bd2d15c050dcba4da1a30d7d7e322d65fc
Helio Pereira <helio.hp71@gmail.com>
2024-05-17 15:32:44 -0300

    Ajuste função sair

--- script.js
+++ script.js
@@ -1,203 +1,68 @@
-// Dados da aplicação.
-const data = Array.from({ length: 100 })
-  .map((_, i) => `Item ${(i + 1)}`);
+const itemsPerPage = 5; // Número de itens por página
+let currentPage = 1;
 
-// Paginas 
-let perPage = 5;
-const state = {
-  page: 1,
-  perPage,
-  totalPage: Math.ceil(data.length / perPage),
-  maxVisibleButtons: 5
-};
+function displayPage(page) {
+    currentPage = page;
+    const start = (page - 1) * itemsPerPage;
+    const end = start + itemsPerPage;
+    const pageItems = denuncias.slice(start, end);
 
-// Pegando elementos da DOM
-const html = {
-  get(element) {
-    return document.querySelector(element);
-  }
-};
+    const tableContainer = document.getElementById("table-container");
+    tableContainer.innerHTML = `
+        <li class="table-header">
+            <div class="col col-1">Nome</div>
+            <div class="col col-2">Nº de Protocolo</div>
+            <div class="col col-3">Denúncia</div>
+            <div class="col col-4">Data da Denúncia</div>
+            <div class="col col-5">Data da Ocorrência</div>
+            <div class="col col-6"></div>
+        </li>`;
 
-
-// Logica dos controles
-const controls = {
-  next() {
-    state.page++;
-
-    // Se o page for maior que o total page, ele vai voltar
-    const lastPage = state.page > state.totalPage;
-    if (lastPage) {
-      state.page--;
-    };
-  },
-
-  prev() {
-    state.page--;
-
-    const firstPage = state.page < 1;
-    if (firstPage) {
-      state.page++;
-    };
-  },
-
-  goTo(page) {
-    // se for passado uma pagina menor que 1, ele vai cai nesse if.
-    if (page < 1) {
-      page = 1;
-    }
-
-    state.page = +page;
-    // se você colocar apenas page, ele não vai ser um numero, quando você coloca um + ele vira um numero.
-
-    // se for passado uma pagina maior que o total de pagina ele cai nesse if.
-    if (page > state.totalPage) {
-      state.page = state.totalPage;
-    }
-
-  },
-
-  createListeners() {
-    html.get('.first').addEventListener('click', () => {
-      controls.goTo(1);
-      update();
+    pageItems.forEach(item => {
+        tableContainer.innerHTML += `
+            <li class="table-row">
+                <div class="col col-1">${item.nome}</div>
+                <div class="col col-2">${item.protocolo}</div>
+                <div class="col col-3">${item.denuncia}</div>
+                <div class="col col-4">${item.dataDenuncia}</div>
+                <div class="col col-5">${item.dataOcorrencia}</div>
+                <div class="col col-6"></div>
+            </li>`;
     });
 
-    html.get('.last').addEventListener('click', () => {
-      controls.goTo(state.totalPage);
-      update();
-    });
-
-    html.get('.prev').addEventListener('click', () => {
-      controls.prev();
-      update();
-    });
-
-    html.get('.next').addEventListener('click', () => {
-      controls.next();
-      update();
-    });
-  }
-};
-
-const list = {
-  create(item) {
-    const div = document.createElement('div');
-    div.classList.add('item');
-    div.innerHTML = item;
-
-    html.get('.list').appendChild(div);
-  },
-  update() {
-    const { perPage } = state;
-
-    // Quando eu entrar ele vai limpar a lista.
-    html.get('.list').innerHTML = '';
-
-    let page = state.page - 1;
-      // estou tirando 1, porque o array começa em 0.
-    let start = page * perPage;
-    let end = start + perPage;
-
-    const paginatedItems = data.slice(start, end);
-    // O primeiro param do slice, e qual variavel eu quero cortar.
-    // O segundo param do slice, e qual a quantidade que eu quero cortar. 
-
-    paginatedItems.forEach(list.create);
-    // para cada item, eu estou jogando no parametro item do create.
-  }
-};
-
-const buttons = {
-  element: html.get('.controls .numbers'),
-  create(number) {
-    const button = document.createElement('div');
-
-    button.innerHTML = number;
-
-    if (state.page == number) {
-      button.classList.add('active');
-    }
-
-    button.addEventListener('click', (event) => {
-      const page = event.target.innerText;
-
-      controls.goTo(page);
-      update();
-    });
-
-    buttons.element.appendChild(button);
-  },
-  update() {
-    // zerando tudo que tem la dentro
-    buttons.element.innerHTML = '';
-
-    const { maxLeft, maxRight } = buttons.calculateMaxVisible();
-
-    for(let page = maxLeft; page <= maxRight; page++) {
-      buttons.create(page);
-    };
-  },
-  calculateMaxVisible() {
-    const { maxVisibleButtons, totalPage } = state;
-
-    let maxLeft = (state.page - Math.floor(maxVisibleButtons / 2));
-    let maxRight = (state.page + Math.floor(maxVisibleButtons / 2));
-    // Math.floor vai arrendondar para baixo.
-
-    if (maxLeft < 1) {
-      maxLeft = 1;
-      maxRight = maxVisibleButtons;
-    };
-
-    if (maxRight > totalPage) {
-      maxLeft = totalPage - (maxVisibleButtons - 1);
-      maxRight = totalPage;
-
-      if (maxLeft < 1) maxLeft = 1;
-    };
-
-    return {maxLeft, maxRight};
-  }
-};
-
-function blockButtons() {
-  const { page, totalPage } = state;
-
-  const elements = {
-    prev: html.get('.controls .prev'),
-    next: html.get('.controls .next'),
-    
-    first: html.get('.controls .first'),
-    last: html.get('.controls .last')
-  };
-
-  if (page == 1) {
-    elements.prev.classList.add('disable');
-    elements.first.classList.add('disable');
-  } else {
-    elements.prev.classList.remove('disable');
-    elements.first.classList.remove('disable');
-  };
-
-  if (page == totalPage) {
-    elements.next.classList.add('disable');
-    elements.last.classList.add('disable');
-  } else {
-    elements.next.classList.remove('disable');
-    elements.last.classList.remove('disable');
-  };
+    updatePagination();
 }
 
-function update() {
-  list.update();
-  buttons.update();
-  controls.createListeners();
-  blockButtons();
-};
+function updatePagination() {
+    const totalPages = Math.ceil(denuncias.length / itemsPerPage);
+    const paginationContainer = document.querySelector(".pagination");
+    paginationContainer.innerHTML = `
+        <li class="page-item ${currentPage === 1 ? 'disabled' : ''}">
+            <a class="page-link" href="#" tabindex="-1" onclick="displayPage(${currentPage - 1})">Anterior</a>
+        </li>`;
 
-// Iniciando Aplicação
-function init() {
-  update();
-};
+    for (let i = 1; i <= totalPages; i++) {
+        paginationContainer.innerHTML += `
+            <li class="page-item ${currentPage === i ? 'active' : ''}" onclick="displayPage(${i})">
+                <a class="page-link" href="#">${i}</a>
+            </li>`;
+    }
 
-window.addEventListener('load', init);
+    paginationContainer.innerHTML += `
+        <li class="page-item ${currentPage === totalPages ? 'disabled' : ''}">
+            <a class="page-link" href="#" onclick="displayPage(${currentPage + 1})">Próximo</a>
+        </li>`;
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    if (!localStorage.getItem('sessionToken')) {
+        window.location.href = 'login.html';
+    } else {
+        displayPage(1);
+    }
+});
+
+function logoff() {
+    localStorage.removeItem('sessionToken');
+    window.location.href = 'login.html';
+}
